343c35bec9437d346c8adf1da3e71158a17f732c
sagitarisandy <97603275+sagitarisandy@users.noreply.github.com>
2024-09-25 16:43:56 +0700

    Tugas 2 C++

---
+++ arya/pert1/main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+// Function untuk mendapatkan harinya
+string getDayFromCode(int kode) {
+    
+    switch(kode) {
+        case 1: return "Senin dari Arya Fadhil Sagitarisandy | 231011400263 | 03TPLE006";
+        case 2: return "Selasa dari Arya Fadhil Sagitarisandy | 231011400263 | 03TPLE006";
+        case 3: return "Rabu dari Arya Fadhil Sagitarisandy | 231011400263 | 03TPLE006";
+        case 4: return "Kamis dari Arya Fadhil Sagitarisandy | 231011400263 | 03TPLE006";
+        case 5: return "Jumat dari Arya Fadhil Sagitarisandy | 231011400263 | 03TPLE006";
+        case 6: return "Sabtu dari Arya Fadhil Sagitarisandy | 231011400263 | 03TPLE006";
+        case 7: return "Minggu dari Arya Fadhil Sagitarisandy | 231011400263 | 03TPLE006";
+        default: return "Kode tidak valid";
+    }
+}
+
+int main() {
+    int kode;
+
+    //Nama Saya
+    cout << "Halo, saya Arya Fadhil Sagitarisandy | 231011400263 | 03TPLE006" << endl;
+    
+    // Inputan dari terminal
+    cout << "Masukkan kode (1-7): ";
+    cin >> kode;
+
+    // Outputnya
+    cout << "Hari: " << getDayFromCode(kode) << endl;
+
+    return 0;
+}
